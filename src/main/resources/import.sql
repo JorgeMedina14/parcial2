f1526d4b1a36df6fb4e7c8c2bdbe7e374fe0eaca
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-05-04 13:00:43 -0600

    Se agrega el proyecto

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into users(name,email,password,createdAt) values ('Jorge','jorgemedina@gmail.com','hola',NOW());
+
+insert into movement(userId, amount, description, type, createdAt) values (1,100,'hola','adios',NOW());
